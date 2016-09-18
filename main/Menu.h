bf933b403015c09c4a44881cdf11794beabc034d
Joel Brandom <afro.hj@gmail.com>
2016-09-18 14:16:42 -0500

    Menu.h

---
+++ main/Menu.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void quitProgram()
+{
+	return;
+}
+
+void addPolynomial();
+
+void showMenu()
+{
+	int option;
+	cout << "Please select an option from below:\n";
+	cout << "0: Add polynomial\n";
+	cout << "1: Quit\n";
+	cin >> option;
+
+	switch (option)
+	{
+	case 0:
+		addPolynomial();
+		break;
+	case 1:
+		quitProgram();
+		break;
+	}
+}
