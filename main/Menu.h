925ecbf05dd73845a10e03c2de2b938e712ee729
Joel Brandom <afro.hj@gmail.com>
2016-09-19 14:52:50 -0500

    Making sure regex works

--- main/Menu.h
+++ main/Menu.h
@@ -22,6 +22,24 @@ void addPolynomial()
 	formatted_input = std::regex_replace(input, find_for_pattern, "$011$2", std::regex_constants::format_default);
 	std::sregex_iterator it(formatted_input.begin(), formatted_input.end(), term_match), it_end;
 
+	while (it != it_end)
+	{
+		bool found = false;
+		std::string coefficient = it->str(1) + it->str(2), exponent = it->str(5) + it->str(6);
+		if(coefficient.empty())
+			coefficient = "1";
+		if(it->str(3).empty() && exponent.empty())
+			exponent = "0";
+		if(!it->str(3).empty() && exponent.empty())
+			exponent = "1";
+
+		if(coefficient != "0")
+		{
+			std::cout << "Co: " << coefficient << ' ' << "Exp: " << exponent << '\n';
+		}
+		++it;
+	}
+
 	showMenu();
 }
 
