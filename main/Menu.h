c3eb9251addfb68907013bdac370fb4f44d41d02
Joel Brandom <afro.hj@gmail.com>
2016-09-23 17:26:39 -0500

    Cleaning up code and clarifying comments

--- main/Menu.h
+++ main/Menu.h
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include <regex>
-//#include "Term.h"
 
 using namespace std;
 
 class Menu
 {
 public:
+	/* Construct a Menu
+	*/
 	Menu() {};
 	void showOptions()
 	{
@@ -31,24 +32,33 @@ public:
 			switch (option)
 			{
 			case 0:
-				addPolynomial2(listOfTerms);
+				addPolynomial(listOfTerms);
 				break;
 			case 1:
-				//quitProgram2();
 				break;
 			}
 		}
 	}
 
-	void addPolynomial2(Polynomial& list)
+	/* Adds user inputted polynomial to Polynomial list
+	@param list The list to add user inputted polynomial to
+	*/
+	void addPolynomial(Polynomial& list)
 	{
 		string input, formatted_input;
 		cout << "Enter polynomial:" << endl;
 		cin >> input;
+
+		/* find_for_pattern regex searches for new line, + or - followed by x or X
+		and regex_replace uses it to place a 1 before terms without explicit coefficients
+		*/
+
+		/* term_match searches for each term of polynomial, after regex_replace all
+		terms now have a coefficient. We can use this constant pattern to find each term
+		*/
 		regex find_for_pattern("(^|\\+|-)([xX])"), term_match("(-)?([0-9]+)([xX])?(\\^(-)?([0-9]+))?");
 		formatted_input = regex_replace(input, find_for_pattern, "$011$2", regex_constants::format_default);
 		sregex_iterator it(formatted_input.begin(), formatted_input.end(), term_match), it_end;
-		//Polynomial list;
 
 		while (it != it_end)
 		{
@@ -65,10 +75,26 @@ public:
 			string coefficient = it->str(1) + it->str(2);
 			string exponent = it->str(5) + it->str(6);
 			char letter = *it->str(3).c_str();
+
+			/* If it->str(1) is not empty but it->str(2) is empty,
+			the coefficient is negative but not explicit, it is -1
+			*/
 			if (!it->str(1).empty() && it->str(2).empty()) coefficient = "-1";
 			if (coefficient.empty()) coefficient = "1";
+
+			/* If the exponent is empty, and there's no x or X,
+			the exponent is actually 0
+			*/
 			if (exponent.empty() && !letter) exponent = "0";
+
+			/* If the exponent is empty, but there is an x or X,
+			the exponent is actually 1
+			*/
 			if (exponent.empty() && letter) exponent = "1";
+
+			/* If the coefficient is not 0, make a Term from the coefficient and exponent
+			and then add it to the list of Terms
+			*/
 			if (coefficient != "0")
 			{
 				Term term(stoi(coefficient), stoi(exponent));
@@ -86,14 +112,17 @@ public:
 		}
 	}
 
-	void outputList(Polynomial & l)
+	/* Outputs Polynomial list as string
+	@param list The Polynomial list to be outputted
+	*/
+	void outputList(Polynomial& list)
 	{
 		compareTermDegree cp;
-		l.sort(cp);
+		list.sort(cp);
 
 		Polynomial::const_iterator it;
 		bool showpos = false;
-		for (it = l.begin(); it != l.end(); ++it)
+		for (it = list.begin(); it != list.end(); ++it)
 		{
 			cout << it->toString(showpos);
 
@@ -104,69 +133,4 @@ public:
 		}
 		cout << endl;
 	}
-
-	/*void quitProgram2()
-	{
-		return;
-	} */
 };
-/*
-void showMenu();
-
-void quitProgram()
-{
-	return;
-}
-
-void addPolynomial()
-{
-	string input, formatted_input;
-	cout << "Enter a polynomial:\n";
-	cin >> input;
-
-	regex find_for_pattern("(^|\\+|-)([xX])"), term_match("(-)?([0-9]+)([xX])?(\\^(-)?([0-9]+))?");
-
-	formatted_input = regex_replace(input, find_for_pattern, "$011$2", regex_constants::format_default);
-	sregex_iterator it(formatted_input.begin(), formatted_input.end(), term_match), it_end;
-
-	while (it != it_end)
-	{
-		bool found = false;
-		std::string coefficient = it->str(1) + it->str(2), exponent = it->str(5) + it->str(6);
-		if (coefficient.empty())
-			coefficient = "1";
-		if (it->str(3).empty() && exponent.empty())
-			exponent = "0";
-		if (!it->str(3).empty() && exponent.empty())
-			exponent = "1";
-
-		if (coefficient != "0")
-		{
-			std::cout << "Co: " << coefficient << ' ' << "Exp: " << exponent << '\n';
-		}
-		++it;
-	}
-
-	showMenu();
-}
-
-void showMenu()
-{
-	int option;
-	cout << "Please select an option from below:\n";
-	cout << "0: Add polynomial\n";
-	cout << "1: Quit\n";
-	cin >> option;
-
-	switch (option)
-	{
-	case 0:
-		addPolynomial();
-		break;
-	case 1:
-		quitProgram();
-		break;
-	}
-}
-
-*/
