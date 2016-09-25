11c64cedb12be6ded7c68d5cd65a95dd395a4caf
Joel Brandom <afro.hj@gmail.com>
2016-09-24 21:18:08 -0500

    Adding xomin1024's comments + cleanup

--- main/Term.h
+++ main/Term.h
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+/*declare Term class*/
+
 class Term
 {
 private:
@@ -13,31 +15,37 @@ public:
 	Term(int co, int exp) : coefficient(co), exponent(exp)
 	{ }
 
+	/*overload < to compare the terms*/
 	bool operator<(Term t) const
 	{
 		return this->exponent < t.exponent;
 	}
 
+	/*overload + to add terms*/
 	void operator+(Term term)
 	{
 		this->coefficient += term.coefficient;
 	}
 
+	/*define exponent function*/
 	int Exponent() const
 	{
 		return this->exponent;
 	}
 
+	/*define coefficient function*/
 	int Coefficient() const
 	{
 		return this->coefficient;
 	}
 
+	/*define addcoefficient function that add coefficent*/
 	void addCoefficient(int co)
 	{
 		coefficient += co;
 	}
 
+	/*define tostring function ,add the sign of coefficient and add exponent*/
 	string toString(bool showpos) const
 	{
 
@@ -63,11 +71,14 @@ public:
 	}
 };
 
+/*define compareTermDegree struct,compare two exponents.*/
 struct compareTermDegree {
 	bool operator()(const Term& lh, const Term& rh) {
 		return lh.Exponent() > rh.Exponent();
 	}
 };
+
+/*declare the list of Polynomial*/
 typedef list<Term> Polynomial;
 
 /* Adds user inputted polynomial to Polynomial list
@@ -171,77 +182,3 @@ void clearPolynomial(Polynomial& list)
 {
 	list.clear();
 }
-
-/*
-void outputList(Polynomial & l)
-{
-	compareTermDegree cp;
-	l.sort(cp);
-
-	Polynomial::const_iterator it;
-	bool showpos = false;
-	for (it = l.begin(); it != l.end(); ++it)
-	{
-		cout << it->toString(showpos);
-
-		if (!showpos)
-		{
-			showpos = true;
-		}
-	}
-	std::cout << endl;
-}
-
-void fillList(std::string& inputt)
-{
-	std::regex find("(^|\\+|-)([xX])"), match("(-)?([0-9]+)([xX])?(\\^(-)?(\\d+))?");
-	std::string input;
-	input = std::regex_replace(inputt, find, "$011$2", std::regex_constants::format_default);
-	std::sregex_iterator it(input.begin(), input.end(), match), it_end;
-	Polynomial list;
-
-	while (it != it_end)
-	{
-		bool found = false;
-		std::string coefficient = it->str(1) + it->str(2);
-		std::string exponent = it->str(5) + it->str(6);
-		char letter = *it->str(3).c_str();
-		if (!it->str(1).empty() && it->str(2).empty()) coefficient = "-1";
-		if (coefficient.empty()) coefficient = "1";
-		if (exponent.empty() && !letter) exponent = "0";
-		if (exponent.empty() && letter) exponent = "1";
-		if (coefficient != "0")
-		{
-			Term term(std::stoi(coefficient), std::stoi(exponent));
-			for (Polynomial::iterator i = list.begin(); i != list.end(); ++i)
-			{
-				if (i->Exponent() == term.Exponent())
-				{
-					found = true;
-					i->addCoefficient(term.Coefficient());
-				}
-			}
-			if (found == false) list.push_back(term);
-		}
-		++it;
-	}
-	outputList(list);
-}
-
-void processTerms()
-{
-	std::string input;
-	std::cin >> input;
-	fillList(input);
-}
-*/
-
-/*
-int main()
-{
-//string str = "2X^2-3X+1";
-string str = "-6x^7-8X^10+2X^2-3X+1-0X^8+11X^21";
-fillList(str);
-return 0;
-}
-*/
