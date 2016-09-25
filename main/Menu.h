d2968d3789ce77129aa199cfbe7d8307fb586a0e
Joel Brandom <afro.hj@gmail.com>
2016-09-25 12:09:01 -0500

    Fixing menu bug

--- main/Menu.h
+++ main/Menu.h
@@ -10,8 +10,77 @@ class Menu
 private:
 	Polynomial listOfTerms;
 public:
-	Menu();
-	void showOptions();
-	void ui_addPolynomial();
-	void ui_clearPolynomial();
+	/* Construct a Menu
+	*/
+	Menu() {};
+	void showOptions()
+	{
+		int option = 0;
+
+		while (option < 2)
+		{
+			cout << "Your current polynomial is: ";
+			if (listOfTerms.empty())
+				cout << "empty\n";
+			else
+				outputList(listOfTerms);
+
+			cout << "Please select an option from below:\n";
+			cout << "0: Add polynomial to current polynomial\n";
+			cout << "1: Clear polynomial\n";
+			cout << "2: Quit\n";
+
+			/* Try user inputted option, if it is not a valid option,
+			then throw an exception and try again
+			*/
+			try
+			{
+				cin >> option;
+				if (!cin)
+					throw invalid_argument("Invalid input.\n");
+			}
+			catch (const invalid_argument& e)
+			{
+				cout << e.what();
+				cin.clear();
+				option = -1;
+			}
+			cin.ignore(numeric_limits<int>::max(), '\n');
+
+			switch (option)
+			{
+				/* Option 0: Add polynomial to current polynomial
+				*/
+				case 0:
+					try
+					{
+						ui_addPolynomial();
+					}
+					catch (const invalid_argument& e)
+					{
+						cout << e.what();
+					}
+					break;
+				/* Option 1: Clear polynomial
+				*/
+				case 1:
+					ui_clearPolynomial();
+					break;
+				/* If option entered is not an available option, do nothing
+				*/
+				default:
+					break;
+			}
+		}
+	}
+
+	void ui_addPolynomial()
+	{
+		addPolynomial(listOfTerms);
+	}
+
+	void ui_clearPolynomial()
+	{
+		clearPolynomial(listOfTerms);
+	}
 };
